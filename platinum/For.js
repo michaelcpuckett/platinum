e39d2ec6c4453ab8d02813759535195d740faddc
MP <mp@mps-macbook-pro.lan>
2020-02-02 05:15:45 -0500

    wip

--- platinum/For.js
+++ platinum/For.js
@@ -17,34 +17,39 @@ export default class PlatinumFor extends PlatinumShadow {
         node.remove()
       })
       each.map(data => {
-        data = { ...data, _host: Object.fromEntries(this.attrs.map(attr => [ attr, this.$store[attr] ])) }
         const shadowEl = window.document.createElement('p-shadow')
         const clone = [...this.fragment.children].find(node => node.key && node.key === data.key) || this.templateContent.cloneNode(true).firstElementChild
-        if (clone.tagName.includes('-')) {
-          Object.assign(clone, data)
-        }
-        Object.entries(data).forEach(([ key, value ]) => {
-          const dataEl = window.document.createElement('data')
-          dataEl.setAttribute('slot', key)
-          dataEl.innerText = value
-          shadowEl.append(dataEl)
+        if (typeof data === 'Object') {
+          data = { ...data, _host: Object.fromEntries(this.attrs.map(attr => [ attr, this.$store[attr] ])) }
+          if (clone.tagName.includes('-')) {
+            Object.assign(clone, data)
+          }
+          Object.entries(data).forEach(([ key, value ]) => {
+            const dataEl = window.document.createElement('data')
+            dataEl.setAttribute('slot', key)
+            dataEl.innerText = value
+            shadowEl.append(dataEl)
 
-          ;[...(clone.matches(`[data-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-attr-${key}]`])].forEach(node => {
-            const attrs = node.getAttribute(`data-attr-${key}`).split(' ')
-            if (value !== undefined && value !== null) {
-              attrs.forEach(attr => node.setAttribute(attr, value))
-            } else {
-              attrs.forEach(attr => node.removeAttribute(attr))
-            }
-          })
-          ;[...(clone.matches(`[data-boolean-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-boolean-attr-${key}]`])].forEach(node => {
-            const attrs = node.getAttribute(`data-boolean-attr-${key}`).split(' ')
-            attrs.forEach(attr => {
-              Object.assign(node, { [attr]: value })
+            ;[...(clone.matches(`[data-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-attr-${key}]`])].forEach(node => {
+              const attrs = node.getAttribute(`data-attr-${key}`).split(' ')
+              if (value !== undefined && value !== null) {
+                attrs.forEach(attr => node.setAttribute(attr, value))
+              } else {
+                attrs.forEach(attr => node.removeAttribute(attr))
+              }
+            })
+            ;[...(clone.matches(`[data-boolean-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-boolean-attr-${key}]`])].forEach(node => {
+              const attrs = node.getAttribute(`data-boolean-attr-${key}`).split(' ')
+              attrs.forEach(attr => {
+                Object.assign(node, { [attr]: value })
+              })
             })
           })
-        })
-
+        } else {
+          const spanEl = window.document.createElement('span')
+          spanEl.innerText = data
+          clone.append(spanEl)
+        }
         shadowEl.shadowRoot.append(clone)
         return shadowEl
       }).forEach(node => this.shadowRoot.append(node))
@@ -61,34 +66,41 @@ export default class PlatinumFor extends PlatinumShadow {
           const each = this.$store[this.each]
           if (Array.isArray(each) && each.length) {
             each.map(data => {
-              data = { ...data, _host: Object.fromEntries(this.attrs.map(attr => [ attr, this.$store[attr] ])) }
               const shadowEl = window.document.createElement('p-shadow')
               const clone = this.templateContent.cloneNode(true).firstElementChild
-              if (clone.tagName.includes('-')) {
-                Object.assign(clone, data)
-              }
 
-              Object.entries(data).forEach(([ key, value ]) => {
-                const dataEl = window.document.createElement('data')
-                dataEl.setAttribute('slot', key)
-                dataEl.innerText = value
-                shadowEl.append(dataEl)
+              if (typeof data === 'Object') {
+                data = { ...data, _host: Object.fromEntries(this.attrs.map(attr => [ attr, this.$store[attr] ])) }
+                if (clone.tagName.includes('-')) {
+                  Object.assign(clone, data)
+                }
 
-                ;[...(clone.matches(`[data-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-attr-${key}]`])].forEach(node => {
-                  const attrs = node.getAttribute(`data-attr-${key}`).split(' ')
-                  if (value !== undefined && value !== null) {
-                    attrs.forEach(attr => node.setAttribute(attr, value))
-                  } else {
-                    attrs.forEach(attr => node.removeAttribute(attr))
-                  }
-                })
-                ;[...(clone.matches(`[data-boolean-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-boolean-attr-${key}]`])].forEach(node => {
-                  const attrs = node.getAttribute(`data-boolean-attr-${key}`).split(' ')
-                  attrs.forEach(attr => {
-                    Object.assign(node, { [attr]: value })
+                Object.entries(data).forEach(([ key, value ]) => {
+                  const dataEl = window.document.createElement('data')
+                  dataEl.setAttribute('slot', key)
+                  dataEl.innerText = value
+                  shadowEl.append(dataEl)
+
+                  ;[...(clone.matches(`[data-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-attr-${key}]`])].forEach(node => {
+                    const attrs = node.getAttribute(`data-attr-${key}`).split(' ')
+                    if (value !== undefined && value !== null) {
+                      attrs.forEach(attr => node.setAttribute(attr, value))
+                    } else {
+                      attrs.forEach(attr => node.removeAttribute(attr))
+                    }
+                  })
+                  ;[...(clone.matches(`[data-boolean-attr-${key}]`) ? [clone] : []), ...clone.querySelectorAll([`[data-boolean-attr-${key}]`])].forEach(node => {
+                    const attrs = node.getAttribute(`data-boolean-attr-${key}`).split(' ')
+                    attrs.forEach(attr => {
+                      Object.assign(node, { [attr]: value })
+                    })
                   })
                 })
-              })
+              } else {
+                const spanEl = window.document.createElement('span')
+                spanEl.innerText = data
+                clone.append(spanEl)
+              }
               shadowEl.shadowRoot.append(clone)
               return shadowEl
             }).forEach(node => this.shadowRoot.append(node))
@@ -100,7 +112,7 @@ export default class PlatinumFor extends PlatinumShadow {
           this.$store.addEventListener(`$change_${attr}`, ({ detail: value }) => {
             ;[...this.shadowRoot.children].forEach((parent) => {
               const node = parent.shadowRoot.firstElementChild
-              if (node.tagName.includes('-')) {
+                if (node.tagName.includes('-')) {
                 Object.assign(node, {
                   [attr]: value
                 })
